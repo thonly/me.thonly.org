9a9af6dcb947b4421ef5b2adc3dbf7de87c7529e
Thon Ly <thonly@heartbank.ceo>
2023-01-28 16:33:14 -0800

    finish share buttons

--- components/tl-body/template.mjs
+++ components/tl-body/template.mjs
@@ -5,22 +5,43 @@ const template = document.createElement("template");
 template.innerHTML = `
     <link rel="stylesheet" href="components/tl-body/shadow.css">
     <slot></slot>
-    <div id="top-left" class="corner"></div>
-    <div id="top-right" class="corner"></div>
-    <div id="bottom-left" class="corner"></div>
-    <div id="bottom-right" class="corner"></div>
-    <img id="language" class="icon" src="components/tl-body/globe.svg">
-    <img id="email" class="icon" src="components/tl-body/mail.svg">
-    <img id="facebook" class="icon" src="components/tl-body/facebook.svg">
-    <img id="twitter" class="icon" src="components/tl-body/twitter.svg">
-    <div class="circle1"></div>
-    <div class="circle2"></div>
-    <div class="circle3"></div>
-    <div class="circle4"></div>
-    <div class="circle5"></div>
-    <div class="circle6"></div>
-    <div class="circle7"></div>
-    <div class="circle8"></div>
+    <aside>
+        <div id="top-left" class="corner"></div>
+        <div id="top-right" class="corner"></div>
+        <div id="bottom-left" class="corner"></div>
+        <div id="bottom-right" class="corner"></div>
+        <img title="English" id="language" class="icon" src="components/tl-body/globe.svg">
+        <a href="mailto:?cc=thonly@siliconwat.dev&bcc=thonly@heartbank.ceo&subject=Thon%20Ly%27s%20Business%20Card&body=Check%20out%20Thon%27s%20Business%20Card%20for%20a%20FREE%20Custom%20static%20website!%0A%0Ahttps://me.thonly.org"><img id="email" class="icon" src="components/tl-body/mail.svg"></a>
+        <div class="fb-share-button" data-href="https://me.thonly.org" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fme.thonly.org%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"><img id="facebook" class="icon" src="components/tl-body/facebook.svg"></a></div>
+        <a target="_blank" class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Check%20out%20%40thonly%20Business%20Card%20for%20a%20FREE%20custom%20static%20website!%0A%0A&url=https://me.thonly.org&hashtags=web,blockchain,ios,consultant"><img id="twitter" class="icon" src="components/tl-body/twitter.svg"></a>
+        <div class="circle1"></div>
+        <div class="circle2"></div>
+        <div class="circle3"></div>
+        <div class="circle4"></div>
+        <div class="circle5"></div>
+        <div class="circle6"></div>
+        <div class="circle7"></div>
+        <div class="circle8"></div>
+        <div id="fb-root"></div>
+    </aside>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0" nonce="kuWZUvKl"></script>
+    <script>
+        window.twttr = (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0],
+                t = window.twttr || {};
+            if (d.getElementById(id)) return t;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
+
+            t._e = [];
+            t.ready = function(f) {
+                t._e.push(f);
+            };
+            return t;
+        }(document, "script", "twitter-wjs"));
+    </script>
 `;
 
 export default template;
