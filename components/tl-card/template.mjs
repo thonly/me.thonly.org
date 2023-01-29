33683eb54aaf52d190ed277983b87a508062aae0
Thon Ly <thonly@heartbank.ceo>
2023-01-28 18:33:41 -0800

    add content

--- components/tl-card/template.mjs
+++ components/tl-card/template.mjs
@@ -1,6 +1,7 @@
 const template = document.createElement("template");
 
 // Reference: https://codepen.io/nazarelen/pen/MedejB?editors=1100
+// https://ttsmp3.com
 
 template.innerHTML = `
     <link rel="stylesheet" href="components/tl-card/shadow.css">
@@ -8,24 +9,33 @@ template.innerHTML = `
         <aside class="card">
             <section class="front">
                 <header>
-                    <img src="components/tl-card/thonly.jpg">
+                    <a href="https://siliconwat.dev"><img class="logo" src="components/tl-card/siliconwat.png"></a>
                 </header>
                 <main>
-                    
+                    <small>Fresno, California</small>
+                    <small>Srae Ambel, Cambodia</small>
                 </main>
                 <footer>
-                    front
+                    <h1 onclick="this.getRootNode().host.speak('siliconwat')">Silicon Wat LLC</h1>
+                    <p>Web, Blockchain, and iOS | Custom Development</p>
+                    <a href="https://siliconwat.dev">siliconwat.dev</a>
                 </footer>
             </section>
             <section class="back">
                 <header>
-                    <img src="components/tl-card/thonly.jpg">
+                    <a href="https://thonly.org"><img class="logo" src="components/tl-card/thonly.jpg"></a>
+                    <button>Book Remote Meeting</button>
                 </header>
                 <main>
-                    
+                    <h1 onclick="this.getRootNode().host.speak('thonly')">Thon Ly</h1>
+                    <p>Lead Architect & Consultant</p>
+                    <!--<p>+1 (562) 726-2681</p>-->
+                    <a href="mailto:thonly@siliconwat.dev">thonly@siliconwat.dev</a>
                 </main>
                 <footer>
-                    back
+                    <img class="social" src="components/tl-card/twitter.svg">
+                    <img class="social" src="components/tl-card/facebook.svg">
+                    <img class="social" src="components/tl-card/youtube.svg">
                 </footer>
             </section>
         </aside>
