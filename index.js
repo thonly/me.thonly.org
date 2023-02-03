212da56318a154fbda091a1f7c3ae608632f91c9
Thon Ly <thonly@heartbank.ceo>
2023-02-03 13:42:23 -0800

    refactor

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import "/components/tl-body/element.mjs";
 import "/components/tl-card/element.mjs";
 
-window.onload = event => document.querySelector('video').style.display = window.chrome ? 'block' : 'none';
+window.onload = event => {
+    const video = document.querySelector('video');
+    video.src = `background${Math.floor(Math.random()*2+1)}.mp4`;
+    video.style.display = window.chrome ? 'block' : 'none';
+}
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
