2ee1caa53fab35af76bcc78d17fe0eb376646ee2
Thon Ly <thonly@heartbank.ceo>
2023-02-04 10:25:13 -0800

    refactor

--- components/tl-card/template.mjs
+++ components/tl-card/template.mjs
@@ -24,13 +24,13 @@ template.innerHTML = `
             <section class="back">
                 <header>
                     <a href="https://thonly.org"><img class="logo" src="components/tl-card/thonly.jpg"></a>
-                    <a href="https://calendar.thonly.org"><button>Book Remote Meeting</button></a>
+                    <a href="https://calendar.thonly.org"><button>Book <b>FREE</b> Consultation</button></a>
                 </header>
                 <main>
                     <h1 onmouseenter="this.getRootNode().host.speak('thonly')" onclick="this.getRootNode().host.speak('thonly')">Thon Ly</h1>
                     <p class="title">Lead Architect & Consultant</p>
                     <p><img class="detail" src="components/tl-card/mail.svg"> <a href="mailto:thonly@siliconwat.dev">thonly@siliconwat.dev</a></p>
-                    <p><img class="detail" src="components/tl-card/voicemail.svg"> +1 (562) 726-2681</p>
+                    <p title="Text Message or Voice Mail ONLY"><img class="detail" src="components/tl-card/voicemail.svg"> +1 (562) 726-2681</p>
                 </main>
                 <footer>
                     <a href="https://twitter.com/siliconwat"><svg class="social" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#575F60" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg></a>
