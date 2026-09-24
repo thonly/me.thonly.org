7e4244508f79053ad827c07797002f6364264efe
Thon Ly <thonly@heartbank.ceo>
2026-09-24 10:18:43 -0700

    Revert "card: based in Kâmpôt; thonly@heartbank.ceo primary, thonly@siliconwat.dev secondary; the three alumni addresses"
    This reverts commit 0774b77190934e8bcf4df049f356eeaa101372aa.

--- components/tl-card/template.mjs
+++ components/tl-card/template.mjs
@@ -29,11 +29,8 @@ template.innerHTML = `
                 <main>
                     <h1 onmouseenter="this.getRootNode().host.speak('thonly')" onclick="this.getRootNode().host.speak('thonly')">Thon Ly</h1>
                     <p class="title">Lead Architect & Consultant</p>
-                    <p class="based">Based in Kâmpôt, Cambodia</p>
-                    <p><img class="detail" src="components/tl-card/mail.svg"> <a href="mailto:thonly@heartbank.ceo">thonly@heartbank.ceo</a></p>
-                    <p class="secondary"><a href="mailto:thonly@siliconwat.dev">thonly@siliconwat.dev</a></p>
+                    <p><img class="detail" src="components/tl-card/mail.svg"> <a href="mailto:thonly@siliconwat.dev">thonly@siliconwat.dev</a></p>
                     <p title="Text Message or Voice Mail ONLY"><img class="detail" src="components/tl-card/voicemail.svg"> +1 (562) 726-2681</p>
-                    <p class="alumni"><a href="mailto:thonly@ucla.edu">thonly@ucla.edu</a><br><a href="mailto:thonly@csu.fullerton.edu">thonly@csu.fullerton.edu</a><br><a href="mailto:thonly@hawaii.edu">thonly@hawaii.edu</a></p>
                 </main>
                 <footer>
                     <a href="https://twitter.com/siliconwat"><svg class="social" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#575F60" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg></a>
